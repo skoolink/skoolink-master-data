f61739f6ab629753b173cb8011a64d25b8ddbd38
wahyu-sty <164998+wahyu-sty@users.noreply.github.com>
2026-09-07 09:09:52 +0000

    Deploying to public from @ skoolink/skoolink-master-data@5ee1e3dfb45c05655a40cf856661cb0ea9374523 🚀

---
+++ schools/schools.js
@@ -0,0 +1,17 @@
+// File ini digenerate otomatis oleh scripts/build_schools.py. Jangan diedit manual.
+export const schools = [
+  {
+    "name": "Sekolah ke Tujuh",
+    "code": "0007",
+    "domain_name": "s0007.com",
+    "logo": "logo/0007.webp"
+  },
+  {
+    "name": "demo skoolink",
+    "code": "3331",
+    "domain_name": "demo.skoolink.id",
+    "logo": "logo/3331.webp"
+  }
+];
+
+export default schools;
